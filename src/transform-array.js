b81fa22303ddb0f2d245725a0ccd4e5c0017d458
Mikhail Shatov <mixachos@gmail.com>
2022-03-22 14:35:12 +0300

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr))
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+
+  if (!arr.includes('--discard-next')
+    && !arr.includes('--discard-prev')
+    && !arr.includes('--double-next')
+    && !arr.includes('--double-prev')) {
+    return arr;
+  }
+
+  const arrCopy = arr.slice();
+
+  for (let i = 0; i < arrCopy.length; i++) {
+    const current = arrCopy[i];
+
+    if (typeof (current) === 'string') {
+      arrCopy[i] = 'del';
+
+      switch (current) {
+        case '--discard-next':
+          arrCopy[i + 1] = 'del';
+          i++;
+          break;
+
+        case '--discard-prev':
+          if (i - 1 >= 0) {
+            arrCopy[i - 1] = 'del';
+          }
+          break;
+
+        case '--double-next':
+          if (i + 1 < arrCopy.length) {
+            arrCopy[i] = arrCopy[i + 1];
+          }
+          break;
+
+        case '--double-prev':
+          if (i - 1 >= 0) {
+            arrCopy[i] = arrCopy[i - 1];
+          }
+          break;
+
+        default:
+          arrCopy[i] = current;
+          break;
+      }
+    }
+  }
+
+  return arrCopy.filter(item => item != 'del');
+
 }
 
 module.exports = {
